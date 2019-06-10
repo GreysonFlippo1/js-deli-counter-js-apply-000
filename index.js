8cfeefe9384f00c79da2d4f0c33acd4514c9b650
Greyson Flippo <greysonflippo@gmail.com>
2019-06-10 02:51:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-var katzDeliLine = [];
+//var katzDeliLine = [];
 var served = 0;
 
-function takeANumber(katzDeliLine,name)
+function takeANumber(deliLine,name)
 {
-  var next = katzDeliLine.length;
-  katzDeliLine[next] = name;
+  var next = deliLine.length;
+  deliLine[next] = name;
   var string1 = "Welcome, "+name+". You are number "+next - served + 1 +" in line.";
   return string1; 
 }
