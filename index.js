645b5507d417af3f1ecfb63d737f5ed8497566b6
Greyson Flippo <greysonflippo@gmail.com>
2019-06-10 03:02:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,10 @@ function currentLine(katzDeliLine){
 } 
 
 function nowServing(katzDeliLine){
-    if(katzDeliLine.length === 0 ){
-    return "The line is currently empty.";
+  if(katzDeliLine.length === 0 ){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return "Currently serving "+katzDeliLine[0];
   }
 }
