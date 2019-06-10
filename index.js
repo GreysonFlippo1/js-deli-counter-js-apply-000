eafd9ec1cbd11141512489df06872f431e47d056
Greyson Flippo <greysonflippo@gmail.com>
2019-06-10 03:01:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 //var katzDeliLine = [];
-var served = 0;
 
 function takeANumber(katzDeliLine,name)
 {
   var next = katzDeliLine.length;
   katzDeliLine[next] = name;
-  var string1 = "Welcome, "+name+". You are number "+(next - served + 1 )+" in line.";
+  var string1 = "Welcome, "+name+". You are number "+(next + 1 )+" in line.";
   return string1; 
 }
 
 function currentLine(katzDeliLine){
-  if(katzDeliLine.length-served === 0 ){
+  if(katzDeliLine.length === 0 ){
     return "The line is currently empty.";
   }
   var string1 = "The line is currently: ";
   for(var i = 0; i < katzDeliLine.length; i++){
-    string1+=(i+1 - served)+". "+katzDeliLine[i];
+    string1+=(i+1)+". "+katzDeliLine[i];
     if(i!=katzDeliLine.length-1){
       string1 += ", ";
     }
   }
   return string1;
-} // "The line is currently: 1. Ada, 2. Grace, 3. Kent"
+} 
+
+function nowServing(katzDeliLine){
+  return"";
+}
