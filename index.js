5b83b2968fe12ffb1f240753112fddbc22c15827
Greyson Flippo <greysonflippo@gmail.com>
2019-06-10 02:52:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var served = 0;
 
 function takeANumber(katzDeliLine,name)
 {
-  var next = deliLine.length;
+  var next = katzDeliLine.length;
   katzDeliLine[next] = name;
   var string1 = "Welcome, "+name+". You are number "+(next - served + 1 )+" in line.";
   return string1; 
